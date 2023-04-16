0240d43da7fd561f994acf7976248ea8df82a76c
melvin <registrosrapel@gmail.com>
2023-04-15 20:18:17 -0600

    cambio

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-const fn = () => {
-    const miString = "soy un string!"
-    let b = miString + " muy feliz"
-    console.log(b)
-}
+using System;
 
-fn()
-
-const clickeado = () => {
-    let a = "chanchito feliz"
-
-    console.log(a)
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a French verb to conjugate:");
+        string verb = Console.ReadLine();
+        
+        Console.WriteLine("Indicatif présent:");
+        Conjugate(verb, "je", "e");
+        Conjugate(verb, "tu", "es");
+        Conjugate(verb, "il/elle/on", "e");
+        Conjugate(verb, "nous", "ons");
+        Conjugate(verb, "vous", "ez");
+        Conjugate(verb, "ils/elles", "ent");
+        
+        Console.WriteLine("Indicatif passé composé:");
+        Conjugate(verb, "j'", "ai ", "e");
+        Conjugate(verb, "tu", "as ", "es");
+        Conjugate(verb, "il/elle/on", "a ", "e");
+        Conjugate(verb, "nous", "avons ", "é");
+        Conjugate(verb, "vous", "avez ", "é");
+        Conjugate(verb, "ils/elles", "ont ", "é");
+        
+        Console.WriteLine("Subjonctif présent:");
+        Conjugate(verb, "que je", "e");
+        Conjugate(verb, "que tu", "es");
+        Conjugate(verb, "qu'il/elle/on", "e");
+        Conjugate(verb, "que nous", "ions");
+        Conjugate(verb, "que vous", "iez");
+        Conjugate(verb, "qu'ils/elles", "ent");
+    }
+    
+    static void Conjugate(string verb, string pronoun, string auxiliary, string ending)
+    {
+        Console.WriteLine($"{pronoun} {auxiliary}{verb}{ending}");
+    }
+    
+    static void Conjugate(string verb, string pronoun, string ending)
+    {
+        Conjugate(verb, pronoun, "", ending);
+    }
 }
